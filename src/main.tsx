23a537eeda97e196fe9fb6bd8e8d4218d8a01a5f
anandachanta19 <anandachanta19@gmail.com>
2025-04-12 15:07:26 +0530

    Add Vercel Analytics and Speed Insights to enhance performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-    <App />,
+    <>
+        <App />
+        <Analytics />
+        <SpeedInsights />
+    </>
 )
